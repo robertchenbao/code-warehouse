45f97086b1220ad5f4827b6b1a7e2736f144de00
robert_bao <ibaochen@gmail.com>
2022-01-11 20:17:28 -0500

    Fix the CORS issue with frontend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    # Resolve the CORS issue with frontend
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
