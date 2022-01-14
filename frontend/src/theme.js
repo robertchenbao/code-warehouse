13ae5845402470b17d16b9e4e1f205e260c89a5a
robert_bao <ibaochen@gmail.com>
2022-01-13 21:48:18 -0500

    Customize app font

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -2,12 +2,16 @@ import { createTheme } from "@mui/material/styles";
 
 export const theme = createTheme({
     palette: {
-        type: "dark",
         primary: {
             main: "#30475e",
         },
         secondary: {
             main: "#F05454",
+        },
+    },
+    typography: {
+        h6: {
+            fontFamily: "Roboto Slab",
         },
     },
 });
