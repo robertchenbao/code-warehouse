d46bdc68477bfba9552bec7274d872020ebd77f7
robert_bao <ibaochen@gmail.com>
2022-01-29 16:24:30 -0500

    Simplify profile UI

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -5,12 +5,16 @@ import { useTheme } from "@mui/material/styles";
 import CodeAppBar from "./CodeAppBar";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-
 import AppBar from "@mui/material/AppBar";
 import Typography from "@mui/material/Typography";
+import LinearProgress from "@mui/material/LinearProgress";
+
+import { useNavigate } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 
 export default function Profile() {
     const theme = useTheme();
+    const navigate = useNavigate();
 
     const [userInfo, setUserInfo] = useState(null);
     const token = localStorage.getItem("jwt_access_token");
@@ -34,8 +38,59 @@ export default function Profile() {
         fetchData();
     }, [token]);
 
-    if (!userInfo) {
-        return null;
+    let currentDate = new Date();
+
+    const displayUserData = (userInfo) => {
+        if (userInfo) {
+            return (
+                <div>
+                    <AppBar position="static" elevation={0}>
+                        <Toolbar className="px-8">
+                            <Typography
+                                variant="h6"
+                                color="inherit"
+                                className="flex-1"
+                            >
+                                General Information
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
+
+                    <CardContent>
+                        <div className="mb-8">
+                            <Typography className="mb-4 text-15">
+                                <b>Username</b>
+                            </Typography>
+                            <Typography>{userInfo.username}</Typography>
+                        </div>
+
+                        <div className="mb-8">
+                            <Typography className="mb-4 text-15">
+                                <b>Email</b>
+                            </Typography>
+                            <Typography>{userInfo.email}</Typography>
+                        </div>
+                    </CardContent>
+                </div>
+            );
+        } else {
+            return (
+                <div className="flex flex-1 flex-col items-center justify-center p-12">
+                    <Typography className="text-20 mb-16" color="textSecondary">
+                        Loading...
+                    </Typography>
+                    <LinearProgress
+                        className="w-xs max-w-full"
+                        color="secondary"
+                    />
+                </div>
+            );
+        }
+    };
+    // Check if the token is present, AND has not expired
+    // otherwise: go back to the main screen
+    if (!token || jwt_decode(token).exp * 1000 < currentDate) {
+        navigate("/");
     }
 
     return (
@@ -66,33 +121,7 @@ export default function Profile() {
                             marginBottom: "16px",
                         }}
                     >
-                        <AppBar position="static" elevation={0}>
-                            <Toolbar className="px-8">
-                                <Typography
-                                    variant="h6"
-                                    color="inherit"
-                                    className="flex-1"
-                                >
-                                    General Information
-                                </Typography>
-                            </Toolbar>
-                        </AppBar>
-
-                        <CardContent>
-                            <div className="mb-8">
-                                <Typography className="mb-4 text-15">
-                                    <b>Username</b>
-                                </Typography>
-                                <Typography>{userInfo.username}</Typography>
-                            </div>
-
-                            <div className="mb-8">
-                                <Typography className="mb-4 text-15">
-                                    <b>Email</b>
-                                </Typography>
-                                <Typography>{userInfo.email}</Typography>
-                            </div>
-                        </CardContent>
+                        {displayUserData(userInfo)}
                     </Card>
                 </Box>
             </div>
